b8cee6bc83a7454d85558860594d93f6e7544786
Bassil-ali <baselali337@gmail.com>
2021-09-19 16:29:51 +0200

    uppercase search

--- app.js
+++ app.js
@@ -68,8 +68,8 @@ document.addEventListener('DOMContentLoaded', function () {
         const term = e.target.value.toLowerCase();
         const books = list.getElementsByTagName('li');
         Array.from(books).forEach((book) => {
-            const title = book.firstElementChild.textContent.toLowerCase();
-            if (title.toLowerCase().indexOf(e.target.value) != -1) {
+            const title = book.firstElementChild.textContent;
+            if (title.toLowerCase().indexOf(term) != -1) {
                 book.style.display = 'block';
             } else {
                 book.style.display = 'none';
